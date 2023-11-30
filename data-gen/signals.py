2bb627e82c68b7247c163a334217aff7ae798663
Mateusz Chwast <chwastmat@gmail.com>
2023-11-30 22:46:32 +0100

    pass min/max frequencies and sample time to generator

--- data-gen/signals.py
+++ data-gen/signals.py
@@ -4,11 +4,20 @@ from abc import ABC, abstractmethod
 class SignalBase(ABC):
 
     @abstractmethod
-    def generate_random(samples : int, amplitude : float):
+    def generate_random(
+        samples : int, 
+        fmin : float, fmax : float, 
+        sample_time_us : float,
+        amplitude : float):
         pass
     
 
 class Sine(SignalBase):
 
-    def generate_random(samples : int, amplitude : float = 1):
+    def generate_random(
+            samples : int, 
+            fmin : float = 10, fmax : float = 1000,
+            sample_time_us : float = 1000, 
+            amplitude : float = 1):
         print("Generating sine")
+
