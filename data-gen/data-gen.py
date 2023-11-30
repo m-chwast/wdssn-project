ab4f8d2c5b0e079703b54707d83faafb750e4ef5
Mateusz Chwast <chwastmat@gmail.com>
2023-12-01 00:45:37 +0100

    print sample sines

--- data-gen/data-gen.py
+++ data-gen/data-gen.py
@@ -1,4 +1,10 @@
 from signals import Sine
+import matplotlib.pyplot as plt
 
-sine = Sine.generate_random(100)
-print(sine)
+
+for i in range(4):
+    sine = Sine.generate_random(100)
+    plt.subplot(4, 1, i + 1)    
+    plt.plot(sine, '.')
+
+plt.show()
