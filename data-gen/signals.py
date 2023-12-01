f86fc20f4dc84e563fecb75356930438e8564e09
Mateusz Chwast <chwastmat@gmail.com>
2023-12-01 01:00:27 +0100

    calculate freq limits in sine gen

--- data-gen/signals.py
+++ data-gen/signals.py
@@ -17,7 +17,7 @@ class SignalBase(ABC):
     def get_freq(fmin : float, fmax : float) -> float:
         return random.random() * (fmax - fmin) + fmin
     
-    def get_freq_limits(observation_time : float) -> (float, float):
+    def get_freq_limits(observation_time : float) -> tuple[float, float]:
         # min freq is a quarter of full signal period
         # max freq is 5 times full signal period 
         fmin = (1 / 4) * (1 / observation_time)
@@ -28,14 +28,18 @@ class Sine(SignalBase):
 
     def generate_random(
             samples : int, 
-            fmin : float = 10, fmax : float = 1000,
             sample_time_us : float = 1000, 
             amplitude : float = 1) -> np.ndarray:
         
-        signal = np.zeros(samples)
+        sample_time = sample_time_us / 1_000_000
+        
+        observation_time = samples * sample_time
+        fmin, fmax = SignalBase.get_freq_limits(observation_time)
         freq = SignalBase.get_freq(fmin, fmax)
+        
+        signal = np.zeros(samples)
         for i in range(0, samples):
-            t = i * sample_time_us / 1_000_000
+            t = i * sample_time
             sample = amplitude * math.sin(2 * math.pi * freq * t)
             signal[i] = sample
         return signal
