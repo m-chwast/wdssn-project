3838d62bd68c0d577a4da565f3a14ac8b159627e
Mateusz Chwast <chwastmat@gmail.com>
2023-12-01 01:38:33 +0100

    move generating to base class

--- data-gen/signals.py
+++ data-gen/signals.py
@@ -1,13 +1,37 @@
 import random
 import math
+from typing import Callable
 import numpy as np
 
 
 class SignalBase:
 
+    def generate_random(
+            samples : int, 
+            sample_time_us : float, 
+            amplitude : float,
+            f : Callable[[float], float]
+        ) -> np.ndarray:
+
+        sample_time = sample_time_us / 1_000_000
+        
+        observation_time = samples * sample_time
+        fmin, fmax = SignalBase.get_freq_limits(observation_time)
+        freq = SignalBase.get_freq(fmin, fmax)        
+        phase_t = (SignalBase.get_phase() / 2 * math.pi) * (1 / freq)
+
+        signal = np.zeros(samples)
+        for i in range(0, samples):
+            t = i * sample_time + phase_t
+            # it's assumed that provided function is periodic with period of 2pi
+            arg = 2 * math.pi * freq * t    
+            sample = amplitude * f(arg)
+            signal[i] = sample
+        return signal        
+
+
     def get_freq(fmin : float, fmax : float) -> float:
         return random.random() * (fmax - fmin) + fmin
-
     
     def get_freq_limits(observation_time : float) -> tuple[float, float]:
         # min freq is a quarter of full signal period
@@ -27,16 +51,12 @@ class Sine(SignalBase):
             sample_time_us : float = 1000, 
             amplitude : float = 1) -> np.ndarray:
         
-        sample_time = sample_time_us / 1_000_000
-        
-        observation_time = samples * sample_time
-        fmin, fmax = SignalBase.get_freq_limits(observation_time)
-        freq = SignalBase.get_freq(fmin, fmax)        
-        phase_t = (SignalBase.get_phase() / 2 * math.pi) * (1 / freq)
-
-        signal = np.zeros(samples)
-        for i in range(0, samples):
-            t = i * sample_time + phase_t
-            sample = amplitude * math.sin(2 * math.pi * freq * t)
-            signal[i] = sample
-        return signal
+        return SignalBase.generate_random(
+            samples, 
+            sample_time_us, 
+            amplitude, 
+            Sine.get_sample)
+    
+    def get_sample(arg) -> float:
+        return math.sin(arg)
+    
