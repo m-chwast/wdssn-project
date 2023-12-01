d38514af6e9ccabdad50d06c4b743259cae1831f
Mateusz Chwast <chwastmat@gmail.com>
2023-12-01 01:15:27 +0100

    add random phase to sine gen

--- data-gen/signals.py
+++ data-gen/signals.py
@@ -7,6 +7,7 @@ class SignalBase:
 
     def get_freq(fmin : float, fmax : float) -> float:
         return random.random() * (fmax - fmin) + fmin
+
     
     def get_freq_limits(observation_time : float) -> tuple[float, float]:
         # min freq is a quarter of full signal period
@@ -14,6 +15,9 @@ class SignalBase:
         fmin = (1 / 4) * (1 / observation_time)
         fmax = 5 * (1 / observation_time)
         return fmin, fmax
+
+    def get_phase() -> float:
+        return 2 * math.pi * random.random()
 
 
 class Sine(SignalBase):
@@ -27,11 +31,12 @@ class Sine(SignalBase):
         
         observation_time = samples * sample_time
         fmin, fmax = SignalBase.get_freq_limits(observation_time)
-        freq = SignalBase.get_freq(fmin, fmax)
-        
+        freq = SignalBase.get_freq(fmin, fmax)        
+        phase_t = (SignalBase.get_phase() / 2 * math.pi) * (1 / freq)
+
         signal = np.zeros(samples)
         for i in range(0, samples):
-            t = i * sample_time
+            t = i * sample_time + phase_t
             sample = amplitude * math.sin(2 * math.pi * freq * t)
             signal[i] = sample
         return signal
