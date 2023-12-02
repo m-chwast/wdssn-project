c14a37fb7db60c973142207fff2f9e4d5d84447f
Mateusz Chwast <chwastmat@gmail.com>
2023-12-03 00:53:55 +0100

    print sample squares

--- data-gen/data-gen.py
+++ data-gen/data-gen.py
@@ -1,10 +1,14 @@
-from signals import Sine
+from signals import Sine, Square
 import matplotlib.pyplot as plt
 
 
 for i in range(4):
     sine = Sine().generate_random(samples=100)
-    plt.subplot(4, 1, i + 1)    
+    plt.subplot(4, 2, i * 2 + 1)    
     plt.plot(sine, '.')
+
+    square = Square().generate_random(samples=100)
+    plt.subplot(4, 2, i * 2 + 2)    
+    plt.plot(square, '.')
 
 plt.show()
