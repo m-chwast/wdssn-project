ce37df76193f50491ded43eb26de7fdca4be9bc4
Mateusz Chwast <chwastmat@gmail.com>
2023-12-03 02:30:54 +0100

    separate plotting from data generation

--- data-gen/data-gen.py
+++ data-gen/data-gen.py
@@ -2,19 +2,31 @@ from signals import Sine, Square, Sawtooth, Triangle
 import matplotlib.pyplot as plt
 
 
-generators = [Sine(), Square(), Triangle(), Sawtooth()]
-gen_cnt = len(generators)
+def plot_sample():
+    generators = [Sine(), Square(), Triangle(), Sawtooth()]
+    gen_cnt = len(generators)
 
-signals = []
-labels = []
+    for i in range(gen_cnt ** 2):
+        signal, label = generators[int(i % gen_cnt)].generate_random(samples=100)
+        plt.subplot(gen_cnt, gen_cnt, i + 1)
+        plt.plot(signal, '.')
 
-for i in range(gen_cnt ** 2):
-    signal, label = generators[int(i % gen_cnt)].generate_random(samples=100)
-    
-    signals.append(signal)
-    labels.append(label)
+    plt.show()
 
-    plt.subplot(gen_cnt, gen_cnt, i + 1)
-    plt.plot(signal, '.')
 
-plt.show()
+def generate():
+    generators = [Sine(), Square(), Triangle(), Sawtooth()]
+    signals = []
+    labels = []
+
+    for i in range(100):
+        signal, label = generators[int(i % len(generators))].generate_random(samples=100)
+        signals.append(signal)
+        labels.append(label)
+
+
+def main():
+    plot_sample()
+    generate()
+
+main()
