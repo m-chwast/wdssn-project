23964fafd5c2ca7d4b145d055f7182d0a38e6718
Mateusz Chwast <chwastmat@gmail.com>
2023-12-03 02:22:29 +0100

    store signals and labels in arrays

--- data-gen/data-gen.py
+++ data-gen/data-gen.py
@@ -5,9 +5,16 @@ import matplotlib.pyplot as plt
 generators = [Sine(), Square(), Triangle(), Sawtooth()]
 gen_cnt = len(generators)
 
+signals = []
+labels = []
+
 for i in range(gen_cnt ** 2):
     wave, label = generators[int(i % gen_cnt)].generate_random(samples=100)
-    plt.subplot(gen_cnt, gen_cnt, i + 1)  
+    
+    signals.append(wave)
+    labels.append(label)
+
+    plt.subplot(gen_cnt, gen_cnt, i + 1)
     plt.plot(wave, '.')
 
 plt.show()
