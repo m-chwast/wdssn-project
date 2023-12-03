9625e867613866430a4df919d11148cd94680d4e
Mateusz Chwast <chwastmat@gmail.com>
2023-12-03 21:16:11 +0100

    read data from csv file

--- network/network.py
+++ network/network.py
@@ -1,1 +1,22 @@
 import numpy as np
+import csv
+
+
+def read_data() -> tuple[np.ndarray, np.ndarray]:
+    with open("data.csv", "r") as file:
+        csv_reader = csv.reader(file)
+
+        data_with_labels = [row for row in csv_reader]
+        # convert the list into a np array
+        data_array = np.array(data_with_labels)
+
+        # separate the array back into data and labels
+        loaded_data = data_array[:, :-1]  # all columns except the last one
+        loaded_labels = data_array[:, -1]  # the last column
+
+        return loaded_data, loaded_labels
+
+def main():
+    data, labels = read_data()
+
+main()
