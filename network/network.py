6f28132b978a1e4db2a51a8d64847c38632eb894
Mateusz Chwast <chwastmat@gmail.com>
2023-12-03 21:25:39 +0100

    enumerate string labels

--- network/network.py
+++ network/network.py
@@ -15,8 +15,15 @@ def read_data() -> tuple[np.ndarray, np.ndarray]:
         loaded_labels = data_array[:, -1]  # the last column
 
         return loaded_data, loaded_labels
+    
+def enumarate_labels(labels_txt : list[str]) -> np.ndarray:
+    string_to_numeric = {string: i for i, string in enumerate(set(labels_txt))}
+    labels_numeric = [string_to_numeric[string] for string in labels_txt]
+    return np.array(labels_numeric)
 
 def main():
-    data, labels = read_data()
+    data, labels_txt = read_data()
 
+    labels = enumarate_labels(labels_txt)
+   
 main()
