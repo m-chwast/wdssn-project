f0a0ea59967404544be3ff66d7ae80f1efb66362
Mateusz Chwast <chwastmat@gmail.com>
2023-12-03 01:23:47 +0100

    generate sample sawtooth and triangle waves

--- data-gen/data-gen.py
+++ data-gen/data-gen.py
@@ -1,14 +1,22 @@
-from signals import Sine, Square
+from signals import Sine, Square, Sawtooth, Triangle
 import matplotlib.pyplot as plt
 
 
 for i in range(4):
     sine = Sine().generate_random(samples=100)
-    plt.subplot(4, 2, i * 2 + 1)  
+    plt.subplot(4, 4, i * 4 + 1)  
     plt.plot(sine, '.')
 
     square = Square().generate_random(samples=100)
-    plt.subplot(4, 2, i * 2 + 2)
+    plt.subplot(4, 4, i * 4 + 2)
     plt.plot(square, '.')
+
+    sawtooth = Sawtooth().generate_random(samples=100)
+    plt.subplot(4, 4, i * 4 + 3)
+    plt.plot(sawtooth, '.')
+
+    triangle = Triangle().generate_random(samples=100)
+    plt.subplot(4, 4, i * 4 + 4)
+    plt.plot(triangle, '.')
 
 plt.show()
