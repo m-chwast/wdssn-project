a850ea2fa741024f67f64b3c3e2970e9ce164262
Mateusz Chwast <chwastmat@gmail.com>
2023-12-03 02:32:11 +0100

    add function for getting generators

--- data-gen/data-gen.py
+++ data-gen/data-gen.py
@@ -2,20 +2,21 @@ from signals import Sine, Square, Sawtooth, Triangle
 import matplotlib.pyplot as plt
 
 
+def get_generators():
+    return [Sine(), Square(), Triangle(), Sawtooth()]
+
 def plot_sample():
-    generators = [Sine(), Square(), Triangle(), Sawtooth()]
+    generators = get_generators()
     gen_cnt = len(generators)
 
     for i in range(gen_cnt ** 2):
         signal, label = generators[int(i % gen_cnt)].generate_random(samples=100)
         plt.subplot(gen_cnt, gen_cnt, i + 1)
         plt.plot(signal, '.')
-
     plt.show()
 
-
 def generate():
-    generators = [Sine(), Square(), Triangle(), Sawtooth()]
+    generators = get_generators()
     signals = []
     labels = []
 
