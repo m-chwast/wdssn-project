ffde025db5d556730a071744942409787a4ef95c
Mateusz Chwast <chwastmat@gmail.com>
2023-12-10 19:26:41 +0100

    add program class, create console

--- embedded/sigclassifier/Core/Program/program.cpp
+++ embedded/sigclassifier/Core/Program/program.cpp
@@ -1,5 +1,27 @@
 #include "program.h"
+#include "usart.h"
+#include "console.h"
 
+
+class Program {
+private:
+
+	Console _console;
+
+public:
+
+	Program(void);
+
+} program;
+
+Program::Program(void)
+	: _console{Console(huart2)} {
+
+}
+
+
+
+// functions to be called from C main
 
 void Program_Init(void) {
 
