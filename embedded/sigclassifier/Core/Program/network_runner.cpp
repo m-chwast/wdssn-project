d76b2868f10e163299bc9e20bdc2137bd3e41509
Mateusz Chwast <chwastmat@gmail.com>
2023-12-14 01:56:04 +0100

    add example of invoke

--- embedded/sigclassifier/Core/Program/network_runner.cpp
+++ embedded/sigclassifier/Core/Program/network_runner.cpp
@@ -28,6 +28,29 @@ void NetworkRunner::Init(void) {
 	    return;
 	}
 
+	_modelInput = _interpreter->input(0);
+	_modelOutput = _interpreter->output(0);
+
+	float inData[100];
+	for(int i = 0; i < 100; i++) {
+		inData[i] = i > 50 ? 1 : 0;
+	}
+
+	for(int i = 0; i < 100; i++) {
+		_modelInput->data.f[i] = inData[i];
+	}
+
+	TfLiteStatus invokeStatus = _interpreter->Invoke();
+	if(invokeStatus != kTfLiteOk) {
+		TF_LITE_REPORT_ERROR(_errorReporter, "Invoke failed\r\n");
+		return;
+	}
+
+	float outData[6];
+	for(int i = 0; i < 6; i++) {
+		outData[i] = _modelOutput->data.f[0];
+	}
+
 	_console << "Network Runner init ok\r\n";
 }
 
